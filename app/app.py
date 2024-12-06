875dc1b1384632786bc87e1841270d6846b2ffa4
AanshOjha <1aanshojha@gmail.com>
2024-12-06 15:36:57 +0530

    fixed error handling

--- app/app.py
+++ app/app.py
@@ -11,6 +11,7 @@ from sqlalchemy import Boolean, Column, DateTime, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
+
 import requests
 from oauthlib.oauth2 import WebApplicationClient
 from pydantic import BaseModel
