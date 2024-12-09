12071dbf561ff0ead38513a8e27018e854c504a4
Adwaith Rajesh <adwaithrajesh3180@gmail.com>
2024-12-09 18:32:52 +0530

    override AND and OR to combine FieldConditions

--- mysql_vector/filter.py
+++ mysql_vector/filter.py
@@ -18,6 +18,27 @@ class Condition(ABC):
     def __call__(self, payload: dict[str, Any]) -> bool:
         return super().__call__(payload)
 
+    def __and__(self, other: Condition) -> Condition:
+        return CombinedCondition(self, other, op="and")
+
+    def __or__(self, other: Condition) -> Condition:
+        return CombinedCondition(self, other, op="or")
+
+
+class CombinedCondition(Condition):
+    def __init__(self, cond1: Condition, cond2: Condition, op: str):
+        if op not in {"and", "or"}:
+            raise ValueError("Invalid operator for combination: must be 'and' or 'or'.")
+        self.cond1 = cond1
+        self.cond2 = cond2
+        self.op = op
+
+    def __call__(self, payload: dict[str, Any]) -> bool:
+        if self.op == "and":
+            return self.cond1(payload) and self.cond2(payload)
+        elif self.op == "or":
+            return self.cond1(payload) or self.cond2(payload)
+
 
 class FieldCondition(Condition):
 
